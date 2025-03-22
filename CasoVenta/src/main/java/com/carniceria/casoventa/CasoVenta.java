dfe75fb5e45e6e9747151c7105433408ecd0d4ff
SebastianMorenoV <erick.moreno252840@potros.itson.edu.mx>
2025-03-21 21:05:50 -0700

    Creacion pantalla realizarVenta del caso de uso
    Empece con la creacion de el boundary para que vayan avanzando

---
+++ CasoVenta/src/main/java/com/carniceria/casoventa/CasoVenta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.carniceria.casoventa;
+
+/**
+ *
+ * @author Lap-064
+ */
+public class CasoVenta {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ RealizarVenta/src/main/java/com/carniceria/realizarventa/RealizarVenta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.carniceria.realizarventa;
+
+/**
+ *
+ * @author Lap-064
+ */
+public class RealizarVenta {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
